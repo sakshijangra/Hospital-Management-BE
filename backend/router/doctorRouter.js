4920843836c438ef3d6d30e5fbaa1b959651330d
Sakshi Jangra <jsakshi619@gmail.com>
2025-05-18 01:45:56 +0530

    changes

--- backend/router/doctorRouter.js
+++ backend/router/doctorRouter.js
@@ -1,30 +1,3 @@
-// import express from "express";
-// import { 
-//     getDoctorAppointments, 
-//     getDoctorPatients, 
-//     getPatientMedicalHistory, 
-//     updateAppointmentStatus as doctorUpdateAppointmentStatus, 
-//     createPrescription, 
-//     getDoctorPrescriptions, 
-//     getDoctorProfile,
-//     logoutDoctor
-// } from "../controller/doctorController.js";
-// import { isDoctorAuthenticated } from "../middlewares/auth.js";
-
-// const router = express.Router();
-
-// // Doctor routes requiring authentication
-// router.get("/appointments", isDoctorAuthenticated, getDoctorAppointments);
-// router.get("/patients", isDoctorAuthenticated, getDoctorPatients);
-// router.get("/patient/:patientId/history", isDoctorAuthenticated, getPatientMedicalHistory);
-// router.put("/appointment/:id/status", isDoctorAuthenticated, doctorUpdateAppointmentStatus);
-// router.post("/prescription/create", isDoctorAuthenticated, createPrescription);
-// router.get("/prescriptions", isDoctorAuthenticated, getDoctorPrescriptions);
-// router.get("/profile", isDoctorAuthenticated, getDoctorProfile);
-// router.get("/logout", isDoctorAuthenticated, logoutDoctor);
-
-// export default router;
-
 import express from "express";
 import { body, param, query } from "express-validator";
 import rateLimit from "express-rate-limit";
